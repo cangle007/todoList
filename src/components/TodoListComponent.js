62a2704185f9b7ed12da6269c6754ff75053f4f3
wildheart007 <cang.b.le@gmail.com>
2018-05-31 22:30:17 -0700

    upgraded ui

--- src/components/TodoListComponent.js
+++ src/components/TodoListComponent.js
@@ -8,68 +8,125 @@ export default class TodoListComponent extends Component {
       showlist: 'DONT_SHOW_LIST',
       showactive: 'DONT_SHOW_ACTIVE',
       showcomplete: 'DONT_SHOW_COMPLETE',
-      completed: false,
-      todosList: ['hello there', 'joker', 'wonderwoman'],
-      todosListCompleted: [],
+      todosList: [
+        { todo: 'kool', completed: false },
+        { todo: 'whtsup', completed: true },
+        { todo: 'getAjob', completed: false }
+      ],
+      displayTodosList: [
+        { todo: 'kool', completed: false },
+        { todo: 'whtsup', completed: true },
+        { todo: 'getAjob', completed: false }
+      ],
       userInput: ''
     };
   }
 
+  // { todo: 'kool', completed: false },
+  // { todo: 'whtsup', completed: true },
+  // { todo: 'getAjob', completed: false }
   showList = event => {
     event.preventDefault();
-    let { showlist } = this.state;
-    if (showlist === 'DONT_SHOW_LIST') {
-      this.setState({ showlist: 'SHOW_LIST' });
-    } else {
-      this.setState({ showlist: 'DONT_SHOW_LIST' });
-    }
+    let { showlist, todosList } = this.state;
+    this.setState({
+      displayTodosList: [...todosList],
+      showlist: 'SHOW_LIST',
+      showactive: 'DONT_SHOW_ACTIVE',
+      showcomplete: 'DONT_SHOW_COMPLETE'
+    });
+
+    // if (showlist === 'DONT_SHOW_LIST') {
+    //   this.setState({ showlist: 'SHOW_LIST' });
+    // } else {
+    //   this.setState({ showlist: 'DONT_SHOW_LIST' });
+    // }
   };
 
   showActive = event => {
     event.preventDefault();
-    let { showactive } = this.state;
-    if (showactive === 'DONT_SHOW_ACTIVE') {
-      this.setState({ showactive: 'SHOW_ACTIVE' });
-    } else {
-      this.setState({ showactive: 'DONT_SHOW_ACTIVE' });
-    }
+    let { showactive, todosList, displayTodosList } = this.state;
+
+    let filtered = todosList.filter(obj => {
+      return obj.completed === false;
+    });
+
+    this.setState({
+      displayTodosList: [...filtered],
+      showactive: 'SHOW_ACTIVE',
+      showcomplete: 'DONT_SHOW_COMPLETE',
+      showlist: 'DONT_SHOW_LIST'
+    });
+
+    // if (showactive === 'DONT_SHOW_ACTIVE') {
+    //   this.setState({ showactive: 'SHOW_ACTIVE' });
+    // } else {
+    //   this.setState({ showactive: 'DONT_SHOW_ACTIVE' });
+    // }
   };
 
   showComplete = event => {
     event.preventDefault();
-    let { showcomplete } = this.state;
-    if (showcomplete === 'DONT_SHOW_COMPLETE') {
-      this.setState({ showcomplete: 'SHOW_COMPLETE' });
-    } else {
-      this.setState({ showcomplete: 'DONT_SHOW_COMPLETE' });
-    }
+    let { showcomplete, todosList, displayTodosList } = this.state;
+    let filtered = todosList.filter(obj => {
+      return obj.completed === true;
+    });
+
+    this.setState({
+      displayTodosList: [...filtered],
+      showcomplete: 'SHOW_COMPLETE',
+      showactive: 'DONT_SHOW_ACTIVE'
+    });
+
+    // if (showcomplete === 'DONT_SHOW_COMPLETE') {
+    //   this.setState({ showcomplete: 'SHOW_COMPLETE' });
+    // } else {
+    //   this.setState({ showcomplete: 'DONT_SHOW_COMPLETE' });
+    // }
   };
 
   handle_completed = event => {
     event.preventDefault();
-    let { completed } = this.state;
-    if (completed === false) {
-      this.setState({ completed: true });
-    } else {
-      this.setState({ completed: false });
-    }
+    let { todosList } = this.state;
+    let id = Number(event.target.id);
+
+    let updated = todosList.map((obj, i) => {
+      if (i === id && obj.completed === false) {
+        obj.completed = true;
+      } else if (i === id && obj.completed === true) {
+        obj.completed = false;
+      }
+      return obj;
+    });
+    this.setState({ todosList: [...updated] });
   };
 
-  deleteTodoList = (event, data) => {
+  handle_deleteTodoList = (event, data) => {
     event.preventDefault();
     let { todosList } = this.state;
-    let deleteListId = Number(event.target.id);
+    let id = Number(event.target.id);
 
-    todosList.splice(deleteListId, 1);
+    for (let i = 0; i < todosList.length; i++) {
+      if (id === i) {
+        todosList.splice(id, 1);
+      }
+    }
+    this.setState({ todosList: todosList });
   };
 
-  handle_AddToLIst = event => {
+  handle_AddTodoList = event => {
     event.preventDefault();
-    let { todosList, userInput } = this.state;
-    let updated = todosList.slice(0);
-    updated.push(userInput);
+    let { todosList, userInput, displayTodosList } = this.state;
+    let obj = { todo: userInput, completed: false };
+    let updatedTodosList = todosList.slice(0);
+    let updatedDisplayTodosList = displayTodosList.slice(0);
 
-    this.setState({ todosList: [...updated], userInput: '' });
+    updatedTodosList.push(obj);
+    updatedDisplayTodosList.push(obj);
+    this.setState({
+      todosList: [...updatedTodosList],
+      displayTodosList: [...updatedDisplayTodosList],
+      userInput: ''
+    });
   };
 
   handle_UserInput = event => {
@@ -83,8 +140,8 @@ export default class TodoListComponent extends Component {
       showactive,
       showcomplete,
       todosList,
-      userInput,
-      completed
+      displayTodosList,
+      userInput
     } = this.state;
 
     return (
@@ -109,21 +166,23 @@ export default class TodoListComponent extends Component {
               transitionName="todo"
               transitionEnterTimeout={500}
               transitionLeaveTimeout={500}>
-              {todosList.map((todo, i) => (
+              {displayTodosList.map((obj, i) => (
                 <li
                   key={i}
-                  onClick={this.handle_completed}
-                  className={'todo' + (completed ? ' todo-completed' : '')}>
-                  <i
-                    className={
-                      'fa ' +
-                      (completed ? 'fa fa-check-circle' : 'fa fa-circle')
-                    }
-                  />
-                  {todo}
+                  id={i}
+                  className={'todo' + (obj.completed ? ' todo-completed' : '')}>
                   <i
                     id={i}
-                    onClick={this.deleteTodoList}
+                    onClick={this.handle_completed}
+                    className={
+                      'fa ' +
+                      (obj.completed ? 'fa fa-check-circle' : 'fa fa-circle')
+                    }
+                  />
+                  {obj.todo}
+                  <i
+                    id={i}
+                    onClick={this.handle_deleteTodoList}
                     className="fa fa-times"
                   />
                 </li>
@@ -138,7 +197,7 @@ export default class TodoListComponent extends Component {
               placeholder="input..."
               onChange={this.handle_UserInput}
             />
-            <button type="submit" onClick={this.handle_AddToLIst}>
+            <button type="submit" onClick={this.handle_AddTodoList}>
               <i className="fa fa-plus" />
             </button>
           </form>
